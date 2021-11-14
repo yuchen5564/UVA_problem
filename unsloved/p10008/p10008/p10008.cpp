438c56c419f86f4dbc0074b8cdfbee756b3cd35d
yuchen5564 <kuo5564@gmail.com>
2021-11-14 12:16:30 +0800

    update p10008(solved)

--- unsloved/p10008/p10008/p10008.cpp
+++ unsloved/p10008/p10008/p10008.cpp
@@ -13,7 +13,8 @@ int main()
 {
     int n = 0;
     string str = {};
-    map<char, int>alphabet;
+    //map<char, int>alphabet;
+    int alphabet[26] = { 0 };
     cin >> n;
     getline(cin, str);
     for (int i = 0; i < n; i++) {
@@ -24,17 +25,35 @@ int main()
         for (int j = 0; j < len; j++) {
             if (str[j] >= 'a' && str[j] <= 'z') {
                 str[j] -= 32;
-                alphabet[str[j]]++;
+                alphabet[str[j]-65]++;
             }
             else if (str[j] >= 'A' && str[j] <= 'Z') {
-                alphabet[str[j]]++;
+                alphabet[str[j]-65]++;
             }
         }
     }
-
-    for (auto i : alphabet) {
-        cout << i.first << " " << i.second << endl;
+    /*for (int i = 0; i < 26; i++) {
+        cout << alphabet[i] << " ";
+    }*/
+    //cout << endl;
+    int max = 0;
+    for (int i = 0; i < 26; i++) {
+        if (alphabet[i] > max) {
+            max = alphabet[i];
+        }
     }
+    //cout << max << endl;
+    for (int i = max; i > 0;i--) {
+        for (int j = 0; j < 26;j++) {
+            if (alphabet[j] == i) {
+                cout << char(j + 65) << " " << alphabet[j] << endl;
+            }
+        }
+    }
+    
+    /*for (auto i : alphabet) {
+        cout << i.first << " " << i.second << endl;
+    }*/
     
     return 0;
 }
